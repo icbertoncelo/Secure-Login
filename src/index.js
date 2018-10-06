b7164b4c4de0de6f24d508a10e8b0d0979394321
Ian Carlos <carlos.ian007@gmail.com>
2018-10-05 22:18:16 -0300

    Add structure and user registration

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express'); //used to manage routes
+const bodyParser = require('body-parser');
+
+const app = express(); // crating aplication 
+
+app.use(bodyParser.json()); //to understand when a json request was sent
+app.use(bodyParser.urlencoded({ extended: false })); //url parameters 
+
+// refering authController
+require('./controllers/authController')(app);
+
+app.listen(3000);
