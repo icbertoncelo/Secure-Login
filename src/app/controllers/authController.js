cf810a57e9357cfa1a480a1e6343958721b6059f
Ian Carlos <carlos.ian007@gmail.com>
2018-10-09 23:02:07 -0300

    Add Password Reset System

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -105,5 +105,37 @@ router.post('/forgot_password', async (req, res) => {
 	}
 });
 
+router.post ('/reset_password', async (req, res) => {
+
+	const { email, token, password } = req.body;
+
+	try{
+
+		const user = await User.findOne({ email })
+			.select('+passwordResetToken passwordResetExpires');
+
+		if(!user)
+			return res.status(400).send({ error: 'User not found' });
+
+		if(token !== user.passwordResetToken)
+			return res.status(400).send({ error: 'Token invalid' });
+
+		const now = new Date();
+
+		if(now > user.passwordResetExpires)
+			return res.status(400).send({ error: 'Token expired! Generate a new one' });	
+
+		user.password = password;
+
+		await user.save();	
+
+		res.send();
+
+	} catch (err) {
+
+ 		res.status(400).send({ error: 'Cannot reset password! Try again'});
+ 		console.log(err);
+	}
+});
 
 module.exports = app => app.use('/auth', router);
