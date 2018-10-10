01c213009664541de9715e8a79682fe9924824a6
Ian Carlos <carlos.ian007@gmail.com>
2018-10-09 22:34:42 -0300

    Add Password Recovery System

---
+++ src/app/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('../../database'); // importing mongoose
+const bcrypt = require('bcryptjs');
+
+// defining Schema - database columns
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,		
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		lowercase: true,
+	},
+	password: {
+		type: String,
+		required: true,
+		select: false, 
+	},
+	passwordResetToken: {
+		type: String,
+		select: false,
+	},
+	passwordResetExpires: {
+		type: Date,
+		select: false,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+
+});
+
+//encrypting the password before saving
+UserSchema.pre('save', async function(next){
+	const hash = await bcrypt.hash(this.password, 10); // generating a hash for the password with 10 rounds
+	this.password = hash;
+
+	next();	
+});
+
+// defining the model
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('express'); //used to manage routes
 const bodyParser = require('body-parser');
 
-const app = express(); // crating aplication 
+const app = express(); // creating aplication 
 
 app.use(bodyParser.json()); //to understand when a json request was sent
 app.use(bodyParser.urlencoded({ extended: false })); //url parameters 
 
 // refering authController
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
+require('./app/controllers/index')(app);
 
 app.listen(3000);
